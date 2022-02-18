9028a8f0874151f83efb560fcee242f50f8ce401
MrBoomDeveloper <mrboomsocial@gmail.com>
2022-02-18 18:21:10 +0500

    changed styles

--- app/src/main/java/com/mrboomdev/androidstudio/MainActivity.java
+++ app/src/main/java/com/mrboomdev/androidstudio/MainActivity.java
@@ -7,7 +7,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.Theme_AndroidStudio);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
