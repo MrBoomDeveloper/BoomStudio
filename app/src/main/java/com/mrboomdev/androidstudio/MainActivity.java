50dd2928b9970cc26e937d21e37b875c515e7f3d
MrBoomDeveloper <mrboomsocial@gmail.com>
2022-02-23 17:07:12 +0500

    changed styles

--- app/src/main/java/com/mrboomdev/androidstudio/MainActivity.java
+++ app/src/main/java/com/mrboomdev/androidstudio/MainActivity.java
@@ -1,7 +1,13 @@
 package com.mrboomdev.androidstudio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
+
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,30 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.Theme_AndroidStudio);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView create = findViewById(R.id.new_project);
+
+        create.setOnClickListener(v -> {
+            PopupMenu popupMenu = new PopupMenu(this, v);
+            popupMenu.inflate(R.menu.create_project);
+            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            switch (item.getItemId()) {
+                                case R.id.create_project:
+                                    Toast.makeText(getApplicationContext(),"create project",
+                                            Toast.LENGTH_SHORT).show();
+                                    return true;
+                                case R.id.open_project:
+                                    Toast.makeText(getApplicationContext(),"open folder",
+                                            Toast.LENGTH_SHORT).show();
+                                    return true;
+                                default:
+                                    return false;
+                            }
+                        }
+                    });
+            popupMenu.show();
+        });
     }
 }
