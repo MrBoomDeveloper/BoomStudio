56c115de0356c7d825214a213e1be2255d2902ef
MrBoomDev <92123190+MrBoomDeveloper@users.noreply.github.com>
2022-02-24 10:32:14 +0500

    update SettingsActivity.java

--- app/src/main/java/com/mrboomdev/androidstudio/SettingsActivity.java
+++ app/src/main/java/com/mrboomdev/androidstudio/SettingsActivity.java
@@ -11,6 +11,12 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        
+        ImageView back = findViewById(R.id.back);
+        
+        back.setOnClickListener(v -> {
+        	finish();
+        });
     }
 
 }
