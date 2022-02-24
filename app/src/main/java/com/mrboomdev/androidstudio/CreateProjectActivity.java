3bf41f68263a84a1168e3a674b8284c8eccac9ef
MrBoomDev <92123190+MrBoomDeveloper@users.noreply.github.com>
2022-02-24 10:34:11 +0500

    update CreateProjectActivity.java

--- app/src/main/java/com/mrboomdev/androidstudio/CreateProjectActivity.java
+++ app/src/main/java/com/mrboomdev/androidstudio/CreateProjectActivity.java
@@ -2,6 +2,8 @@ package com.mrboomdev.androidstudio;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class CreateProjectActivity extends AppCompatActivity {
 
@@ -9,6 +11,12 @@ public class CreateProjectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_project);
+        
+        ImageView back = findViewById(R.id.back);
+        
+        back.setOnClickListener(v -> {
+        	finish();
+        });
     }
 
 }
