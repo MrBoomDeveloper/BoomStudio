5330d45b45795b02c1e2472ab3f15ef35a34554e
MrBoomDev <92123190+MrBoomDeveloper@users.noreply.github.com>
2022-04-09 16:05:28 +0500

    Update ProjectsListAdapter.java

--- app/src/main/java/com/mrboomdev/androidstudio/ProjectsListAdapter.java
+++ app/src/main/java/com/mrboomdev/androidstudio/ProjectsListAdapter.java
@@ -11,27 +11,40 @@ import java.util.List;
 import java.util.Map;
 
 public class ProjectsListAdapter extends RecyclerView.Adapter<ProjectsListAdapter.ViewHolder> {
+	private ArrayList<ProjectItem> mProjectsList;
 
 	public class ViewHolder extends RecyclerView.ViewHolder {
+		public TextView name;
+		public TextView path;
+		public ImageView options;
 
         public ViewHolder(View view) {
             super(view);
+            name = view.findViewById(R.id.name);
+            path = view.findViewById(R.id.path);
+            options = view.findViewById(R.id.options);
         }
-        
     }
+    
+    public ProjectsListAdapter(ArrayList<ProjectItem> list) {
+    	mProjectsList = list;
+	}
 
     @Override
-    public ProjectsListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ProjectsListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_project, parent, false);
+        return new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(ProjectsListAdapter.ViewHolder holder, int position) {
-        
+        ProjectItem currentItem = mProjectsList.get(position);
+        holder.mName.setText(currentItem.getName());
+        holder.mPath.setText(currentItem.getPath());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mProjectsList.size();
     }
 }
