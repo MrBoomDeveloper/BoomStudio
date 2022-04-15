84b5762c3465840a2f1a528789f911c91f475714
MrBoomDev <92123190+MrBoomDeveloper@users.noreply.github.com>
2022-04-15 11:30:20 +0500

    update File.java

--- app/src/main/java/com/mrboomdev/androidstudio/utils/File.java
+++ app/src/main/java/com/mrboomdev/androidstudio/utils/File.java
@@ -2,4 +2,34 @@ package com.mrboomdev.androidstudio.utils;
 
 public class File {
 	
+	public void writeFile(String path, String content) {
+		
+	}
+	
+	public void writeFolder(String path) {
+		
+	}
+	
+	public void deletePath(String path) {
+		
+	}
+	
+	public boolean isExist(String path) {
+		if(1 ==  2) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void ArrayList(String path) {
+		return null;
+	}
+	
+	public boolean unzip(String from, String to, boolean isAsset) {
+		return false;
+	}
+	
+	public boolean zip(String from, String to) {
+		return false;
+	}
 }
