56abd34b6df1f89f3d6b9df3224a38285ead4010
MrBoomDev <92123190+MrBoomDeveloper@users.noreply.github.com>
2022-04-15 08:32:08 +0500

    update File.java

--- app/src/main/java/com/mrboomdev/androidstudio/utils/File.java
+++ app/src/main/java/com/mrboomdev/androidstudio/utils/File.java
@@ -1,24 +1,5 @@
 package com.mrboomdev.androidstudio.utils;
 
-import android.util.filestream.io;
-import java.util.fileEncoding;
-
 public class File {
 	
-	FileStream io = new FileStream();
-	FileEncoding encoding = new FileEncoding(utf8, testRun);
-	public boolean writeFile(String path, String content) {
-		io.open();
-		io.writeFile(encoding, path, content);
-		while(io.open()) {
-			if(io.content().pos() == io.length()) {
-				io.close();
-			}
-		}
-		io.openPath(path);
-		if(io.content() != null) {
-			return true;
-		}
-		return false;
-	}
 }
