8d1bda8f516339a4ce9c8a664f16253abe56c997
MrBoomDev <92123190+MrBoomDeveloper@users.noreply.github.com>
2022-04-17 03:14:31 +0500

    update ProjectsListAdapter.java

--- app/src/main/java/com/mrboomdev/androidstudio/ProjectsListAdapter.java
+++ app/src/main/java/com/mrboomdev/androidstudio/ProjectsListAdapter.java
@@ -7,6 +7,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class ProjectsListAdapter extends RecyclerView.Adapter<ProjectsListAdapter.ProjectsViewHolder> {
 	private ArrayList<ProjectItem> mProjectsList;
