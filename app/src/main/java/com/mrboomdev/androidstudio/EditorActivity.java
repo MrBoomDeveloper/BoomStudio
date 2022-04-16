61cd080559f0273193a816a40606244c01c92893
MrBoomDev <92123190+MrBoomDeveloper@users.noreply.github.com>
2022-04-17 04:20:43 +0500

    update EditorActivity.java

--- app/src/main/java/com/mrboomdev/androidstudio/EditorActivity.java
+++ app/src/main/java/com/mrboomdev/androidstudio/EditorActivity.java
@@ -2,6 +2,8 @@ package com.mrboomdev.androidstudio;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+import android.content.Intent;
 
 public class EditorActivity extends AppCompatActivity {
 
@@ -9,6 +11,11 @@ public class EditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+        
+        Intent intent = getIntent();
+        TextView title = findViewById(R.id.title);
+        
+        title.setText(intent.getStringExtra("name"));
     }
 
 }
