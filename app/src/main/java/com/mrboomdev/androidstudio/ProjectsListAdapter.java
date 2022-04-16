8ade41e7e500788c028d1b746d52d78f6f1764e4
MrBoomDev <92123190+MrBoomDeveloper@users.noreply.github.com>
2022-04-17 03:10:25 +0500

    update ProjectsListAdapter.java

--- app/src/main/java/com/mrboomdev/androidstudio/ProjectsListAdapter.java
+++ app/src/main/java/com/mrboomdev/androidstudio/ProjectsListAdapter.java
@@ -9,39 +9,57 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 public class ProjectsListAdapter extends RecyclerView.Adapter<ProjectsListAdapter.ProjectsViewHolder> {
-    private ArrayList<ProjectItem> mProjectsList;
+	private ArrayList<ProjectItem> mProjectsList;
+	private onItemClickListener mListener;
+	
+	public interface onItemClickListener {
+		void onItemClick(int position);
+	}
+   
+   public void setOnItemClickListener(OnItemClickListener listener) {
+   	mListener = listener;
+   }
 
-    public static class ProjectsViewHolder extends RecyclerView.ViewHolder {
-        public TextView mName;
-        public TextView mPath;
+	public static class ProjectsViewHolder extends RecyclerView.ViewHolder {
+		public TextView mName;
+		public TextView mPath;
 
-        public ProjectsViewHolder(View view) {
-            super(view);
-            mName = view.findViewById(R.id.name);
-            mPath = view.findViewById(R.id.path);
-        }
-    }
+		public ProjectsViewHolder(View view, OnItemClickListener listener) {
+			super(view);
+			mName = view.findViewById(R.id.name);
+			mPath = view.findViewById(R.id.path);
+			
+			view.setOnClickListener(v -> {
+				if(listener != null) {
+					int position = getAdapterPosition();
+					if(position != RecyclerView.NO_POSITION) {
+						listener.onItemClick(position);
+					}
+				}
+			});
+		}
+	}
 
-    public ProjectsListAdapter(ArrayList<ProjectItem> list) {
-        mProjectsList = list;
-    }
+	public ProjectsListAdapter(ArrayList<ProjectItem> list) {
+		mProjectsList = list;
+	}
 
-    @Override
-    public ProjectsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_project, parent, false);
-        ProjectsViewHolder evh = new ProjectsViewHolder(v);
-        return evh;
-    }
+	@Override
+	public ProjectsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+		View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_project, parent, false);
+		ProjectsViewHolder evh = new ProjectsViewHolder(v, mListener);
+		return evh;
+	}
 
-    @Override
-    public void onBindViewHolder(ProjectsViewHolder holder, int position) {
-        ProjectItem currentItem = mProjectsList.get(position);
-        holder.mName.setText(currentItem.getName());
-        holder.mPath.setText(currentItem.getPath());
-    }
+	@Override
+	public void onBindViewHolder(ProjectsViewHolder holder, int position) {
+		ProjectItem currentItem = mProjectsList.get(position);
+		holder.mName.setText(currentItem.getName());
+		holder.mPath.setText(currentItem.getPath());
+	}
 
-    @Override
-    public int getItemCount() {
-        return mProjectsList.size();
-    }
+	@Override
+	public int getItemCount() {
+		return mProjectsList.size();
+	}
 }
