5f4d8b317b98b66478217ba8af85f29fec07f465
MrBoomDev <92123190+MrBoomDeveloper@users.noreply.github.com>
2022-04-21 23:29:34 +0500

    update Files.java

--- app/src/main/java/com/mrboomdev/androidstudio/utils/Files.java
+++ app/src/main/java/com/mrboomdev/androidstudio/utils/Files.java
@@ -208,7 +208,7 @@ public class Files {
     
     public String uriToPath(String uri) {
     	String uri = uri.replace("content://com.android.providers.downloads.documents/tree/raw%3A%2F", "/storage/emulated/0/Downloads/").replace("content://com.android.externalstorage.documents/tree/primary", "/storage/emulated/0/").replace("content://com.android.externalstorage.documents/tree", "/storage/");
-		uri = uri.replace("%3A", "/"). replace("%2F", "/"));
+		uri = uri.replace("%3A", "/").replace("%2F", "/"));
     return uri;
     }
 }
