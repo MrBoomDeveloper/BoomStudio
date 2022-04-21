49ce6e95a4546174c58da2798ecb0b5a4288c86b
MrBoomDev <92123190+MrBoomDeveloper@users.noreply.github.com>
2022-04-21 21:34:54 +0500

    update CreateProjectActivity.java

--- app/src/main/java/com/mrboomdev/androidstudio/CreateProjectActivity.java
+++ app/src/main/java/com/mrboomdev/androidstudio/CreateProjectActivity.java
@@ -6,9 +6,12 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.google.android.material.textfield.TextInputLayout;
+import com.mrboomdev.androidstudio.utils.Files;
 
 public class CreateProjectActivity extends AppCompatActivity {
-
+	Files file;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +19,7 @@ public class CreateProjectActivity extends AppCompatActivity {
 
         String[] langs = {"Java", "Kotlin"};
         String[] sdks = {
+        		"Android 13 (API level 33)",
                 "Android 12L (API level 32)",
                 "Android 12 (API level 31)",
                 "Android 11 (API level 30)",
@@ -30,11 +34,17 @@ public class CreateProjectActivity extends AppCompatActivity {
                 "Android 5.0 (API level 21)"};
         ImageView close = findViewById(R.id.close);
         TextView create = findViewById(R.id.create);
+        
+        TextInputLayout name = findViewById(R.id.project_name);
         AutoCompleteTextView lang = findViewById(R.id.langs);
         AutoCompleteTextView sdk = findViewById(R.id.sdk);
 
         create.setOnClickListener(v -> {
-            // TODO: 27.02.2022
+            if(file.writeFile(name.getText().toString(), "test.txt", "hello world!")) {
+				Toast.makeText(getApplicationContext(),"yay!", Toast.LENGTH_SHORT).show();
+			} else {
+				Toast.makeText(getApplicationContext(),"error :(", Toast.LENGTH_SHORT).show();
+			}
         });
         close.setOnClickListener(v -> finish());
 
