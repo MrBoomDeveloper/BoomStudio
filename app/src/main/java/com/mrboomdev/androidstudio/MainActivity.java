e3103504f06fe0b429b3fa4f177e03710f37ba74
MrBoomDev <92123190+MrBoomDeveloper@users.noreply.github.com>
2022-04-26 13:29:11 +0500

    update MainActivity.java

--- app/src/main/java/com/mrboomdev/androidstudio/MainActivity.java
+++ app/src/main/java/com/mrboomdev/androidstudio/MainActivity.java
@@ -51,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		//Initialize objects
+		
 		refresh = findViewById(R.id.refresh);
 		no_projects = findViewById(R.id.no_projects);
 		projects_recycler = findViewById(R.id.projects_recycler);
@@ -61,7 +63,28 @@ public class MainActivity extends AppCompatActivity {
 		ImageView settings = findViewById(R.id.settings);
 		NavigationBarView navigation = findViewById(R.id.navigation);
 		prefs = getSharedPreferences("projects", Context.MODE_PRIVATE);
-
+		file = new Files();
+		initializeClicks();
+		
+		//Style activity
+		
+		projects_recycler.setLayoutManager(new LinearLayoutManager(this));
+		projects_recycler.setHasFixedSize(true);
+		projects_recycler.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+		if((getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES)
+			refresh.setProgressBackgroundColorSchemeColor(Color.parseColor("#212121"));
+		refresh.setColorSchemeColors(Color.parseColor("#39A8FF"));
+		refresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+			@Override
+			public void onRefresh() {
+				listProjects();
+				}
+		});
+		listProjects();
+	}
+	
+	
+	public void initializeClicks() {
 		notifications.setOnClickListener(v -> {
 			Intent intent = new Intent(getApplicationContext(), NotificationsActivity.class);
 			startActivity(intent);
@@ -69,6 +92,36 @@ public class MainActivity extends AppCompatActivity {
 		settings.setOnClickListener(v -> {
 			Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
 			startActivity(intent);
+		});
+		create.setOnClickListener(v -> {
+			Intent intent = new Intent(getApplicationContext(), CreateProjectActivity.class);
+			startActivity(intent);
+		});
+		open.setOnClickListener(v -> {
+			Intent i = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+			startActivityForResult(i, 2);
+		});
+		new_project.setOnClickListener(v -> {
+			PopupMenu popupMenu = new PopupMenu(this, v);
+			popupMenu.inflate(R.menu.create_project);
+			popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+				@Override
+				public boolean onMenuItemClick(MenuItem item) {
+					switch (item.getItemId()) {
+						case R.id.create_project:
+							create.performClick();
+							break;
+						case R.id.open_project:
+							open.performClick();
+							break;
+						case R.id.get_from_vcs:
+							Toast.makeText(getApplicationContext(),"currently not available1", Toast.LENGTH_SHORT).show();
+							break;
+					}
+					return true;
+				}
+			});
+			popupMenu.show();
 		});
 		navigation.setOnItemSelectedListener(item -> {
 			switch(item.getItemId()) {
@@ -87,55 +140,10 @@ public class MainActivity extends AppCompatActivity {
 				}
 			return true;
 		});
-		projects_recycler.setLayoutManager(new LinearLayoutManager(this));
-		projects_recycler.setHasFixedSize(true);
-		projects_recycler.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
-		file = new Files();
-		checkPermission();
 	}
 
 	public void checkPermission() {
 		if(ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-			create.setOnClickListener(v -> {
-				Intent intent = new Intent(getApplicationContext(), CreateProjectActivity.class);
-				startActivity(intent);
-			});
-			open.setOnClickListener(v -> {
-				Intent i = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-				startActivityForResult(i, 2);
-			});
-			new_project.setOnClickListener(v -> {
-				PopupMenu popupMenu = new PopupMenu(this, v);
-				popupMenu.inflate(R.menu.create_project);
-				popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-					@Override
-					public boolean onMenuItemClick(MenuItem item) {
-						switch (item.getItemId()) {
-							case R.id.create_project:
-								create.performClick();
-								break;
-							case R.id.open_project:
-								open.performClick();
-								break;
-							case R.id.get_from_vcs:
-								Toast.makeText(getApplicationContext(),"currently not available1", Toast.LENGTH_SHORT).show();
-								break;
-						}
-						return true;
-					}
-				});
-				popupMenu.show();
-			});
-			if((getResources().getConfiguration().uiMode &
-	Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES)
-				refresh.setProgressBackgroundColorSchemeColor(Color.parseColor("#212121"));
-			refresh.setColorSchemeColors(Color.parseColor("#39A8FF"));
-			refresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-				@Override
-				public void onRefresh() {
-					listProjects();
-				}
-			});
 			listProjects();
 		} else {
 			new MaterialAlertDialogBuilder(MainActivity.this)
@@ -154,37 +162,35 @@ public class MainActivity extends AppCompatActivity {
 	}
 
 	public void listProjects() {
-		//Load projects list from storage
-		
-		Map<String,?> keys = prefs.getAll();
-		ArrayList<ProjectItem> projects_list = new ArrayList<>();
-		for(Map.Entry<String,?> entry : keys.entrySet()){
-			projects_list.add(new ProjectItem("Java", entry.getKey(), entry.getValue().toString()));
-		}
-		//Display projects list
-		
-		if(projects_list.size() > 0) {
-			no_projects.setVisibility(View.GONE);
-			projects_recycler.setVisibility(View.VISIBLE);
-			adapter = new ProjectsListAdapter(projects_list);
-			projects_recycler.setAdapter(adapter);
-			adapter.setOnItemClickListener(new ProjectsListAdapter.OnItemClickListener() {
-				@Override
-				public void onItemClick(int position) {
-					Intent intent = new Intent(getApplicationContext(), EditorActivity.class);
-					intent.putExtra("name", projects_list.get(position).getName());
-					intent.putExtra("path", projects_list.get(position).getPath());
-					startActivity(intent);
-				}
-			});
+		if(ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+			Map<String,?> keys = prefs.getAll();
+			ArrayList<ProjectItem> projects_list = new ArrayList<>();
+			for(Map.Entry<String,?> entry : keys.entrySet()){
+				projects_list.add(new ProjectItem("Java", entry.getKey(), entry.getValue().toString()));
+			}
+			if(projects_list.size() > 0) {
+				no_projects.setVisibility(View.GONE);
+				projects_recycler.setVisibility(View.VISIBLE);
+				adapter = new ProjectsListAdapter(projects_list);
+				projects_recycler.setAdapter(adapter);
+				adapter.setOnItemClickListener(new ProjectsListAdapter.OnItemClickListener() {
+					@Override
+					public void onItemClick(int position) {
+						Intent intent = new Intent(getApplicationContext(), EditorActivity.class);
+						intent.putExtra("name", projects_list.get(position).getName());
+						intent.putExtra("path", projects_list.get(position).getPath());
+						startActivity(intent);
+					}
+				});
+			} else {
+				no_projects.setVisibility(View.VISIBLE);
+				projects_recycler.setVisibility(View.GONE);
+			}
+			refresh.setRefreshing(false);
 		} else {
-			no_projects.setVisibility(View.VISIBLE);
-			projects_recycler.setVisibility(View.GONE);
+			checkPermission();
 		}
-		refresh.setRefreshing(false);
 	}
-	
-	
 
 	@Override
 	public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
