20ca375b5ece1af189d49a4867b556b21062aa78
MrBoomDev <92123190+MrBoomDeveloper@users.noreply.github.com>
2022-05-01 16:36:53 +0500

    update EditorActivity.java

--- app/src/main/java/com/mrboomdev/androidstudio/EditorActivity.java
+++ app/src/main/java/com/mrboomdev/androidstudio/EditorActivity.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.content.res.Configuration;
 import android.content.Intent;
 import android.graphics.Color;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
@@ -18,26 +21,26 @@ public class EditorActivity extends AppCompatActivity {
 	ImageView menu;
 	ImageView drawer_import_file;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_editor);
-        
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_editor);
+		
 		TextView title = findViewById(R.id.title);
-        intent = getIntent();
-        drawer_layout = findViewById(R.id.drawer_layout);
-        drawer = findViewById(R.id.drawer);
-        drawer_import_file = drawer.findViewById(R.id.upload_file);
+		intent = getIntent();
+		drawer_layout = findViewById(R.id.drawer_layout);
+		drawer = findViewById(R.id.drawer);
+		drawer_import_file = drawer.findViewById(R.id.upload_file);
 		menu = findViewById(R.id.menu);
-        refresh = findViewById(R.id.refresh);
-        
-        title.setText(intent.getStringExtra("name"));
+		refresh = findViewById(R.id.refresh);
+		
+		title.setText(intent.getStringExtra("name"));
 		if((getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES)
 			refresh.setProgressBackgroundColorSchemeColor(Color.parseColor("#212121"));
 		refresh.setColorSchemeColors(Color.parseColor("#39A8FF"));
 		initializeClicks();
-    }
-    
+	}
+	
 	public void initializeClicks() {
 		menu.setOnClickListener(v -> drawer_layout.openDrawer(GravityCompat.START));
 		drawer_import_file.setOnClickListener(v -> {
@@ -45,14 +48,13 @@ public class EditorActivity extends AppCompatActivity {
 		});
 		refresh.setOnRefreshListener(() -> refresh.setRefreshing(false));
 	}
-    
-    @Override
-	public void onBackPressed() {
-		if (drawer.isDrawerOpen(GravityCompat.START)) {
-			drawer.closeDrawer(GravityCompat.START);
-		} else {
-			super.onBackPressed();
-		}
-	}
 
+	@Override
+	public void onBackPressed() {
+		if (drawer.isDrawerOpen(GravityCompat.START)) {
+			drawer.closeDrawer(GravityCompat.START);
+		} else {
+			super.onBackPressed();
+		}
+	}
 }
