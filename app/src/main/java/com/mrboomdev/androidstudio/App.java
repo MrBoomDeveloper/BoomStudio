52aa824a9f1aecab6dbb0f67de9141f76ce3f468
MrBoomDev <92123190+MrBoomDeveloper@users.noreply.github.com>
2022-05-16 23:59:55 +0500

    update App.java

---
+++ app/src/main/java/com/mrboomdev/androidstudio/App.java
@@ -0,0 +1,33 @@
+package com.mrboomdev.test;
+
+import android.app.Application;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Process;
+import android.util.Log;
+
+public class SketchApplication extends Application {
+    private Thread.UncaughtExceptionHandler uncaughtExceptionHandler;
+
+    @Override
+    public void onCreate() {
+        this.uncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
+
+		Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+			@Override
+			public void uncaughtException(Thread thread, Throwable throwable) {
+				Intent intent = new Intent(getApplicationContext(), CrashActivity.class);
+				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+				intent.putExtra("error", Log.getStackTraceString(throwable));
+				PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 11111, intent, PendingIntent.FLAG_ONE_SHOT);
+
+				Process.killProcess(Process.myPid());
+				System.exit(1);
+
+				uncaughtExceptionHandler.uncaughtException(thread, throwable);
+			}
+		});
+		super.onCreate();
+	}
+}
