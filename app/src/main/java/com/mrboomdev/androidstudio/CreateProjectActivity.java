36216e5721d97879d28d83f1b209a8a55fda0b9a
MrBoomDev <92123190+MrBoomDeveloper@users.noreply.github.com>
2022-05-17 19:32:15 +0500

    update CreateProjectActivity.java

--- app/src/main/java/com/mrboomdev/androidstudio/CreateProjectActivity.java
+++ app/src/main/java/com/mrboomdev/androidstudio/CreateProjectActivity.java
@@ -13,46 +13,47 @@ import com.mrboomdev.androidstudio.utils.Files;
 public class CreateProjectActivity extends AppCompatActivity {
 	Files file;
 	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_project);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_create_project);
 
-        String[] langs = {"Java", "Kotlin"};
-        String[] sdks = {
-        		"Android 13 (API level 33)",
-                "Android 12L (API level 32)",
-                "Android 12 (API level 31)",
-                "Android 11 (API level 30)",
-                "Android 10 (API level 29)",
-                "Android 9 (API level 28)",
-                "Android 8.1 (API level 27)",
-                "Android 8.0 (API level 26)",
-                "Android 7.1 (API level 25)",
-                "Android 7.0 (API level 24)",
-                "Android 6.0 (API level 23)",
-                "Android 5.1 (API level 22)",
-                "Android 5.0 (API level 21)"};
-        ImageView close = findViewById(R.id.close);
-        TextView create = findViewById(R.id.create);
-        
-        TextInputEditText name = findViewById(R.id.project_name);
-        AutoCompleteTextView lang = findViewById(R.id.langs);
-        AutoCompleteTextView sdk = findViewById(R.id.sdk);
+		String[] langs = {"Java", "Kotlin"};
+		String[] sdks = {
+				"Android 13 (API level 33)",
+				"Android 12L (API level 32)",
+				"Android 12 (API level 31)",
+				"Android 11 (API level 30)",
+				"Android 10 (API level 29)",
+				"Android 9 (API level 28)",
+				"Android 8.1 (API level 27)",
+				"Android 8.0 (API level 26)",
+				"Android 7.1 (API level 25)",
+				"Android 7.0 (API level 24)",
+				"Android 6.0 (API level 23)",
+				"Android 5.1 (API level 22)",
+				"Android 5.0 (API level 21)"};
+		ImageView close = findViewById(R.id.close);
+		TextView create = findViewById(R.id.create);
+	
+		TextInputEditText name = findViewById(R.id.project_name);
+		TextInputEditText project_package = findViewById(R.id.project_path);
+		AutoCompleteTextView lang = findViewById(R.id.langs);
+		AutoCompleteTextView sdk = findViewById(R.id.sdk);
 
-        create.setOnClickListener(v -> {
-			if(file.writeFile(file.uriToPath("/storage/emulated/0" + "/test.txt"), "hello world!")) {
-				Toast.makeText(getApplicationContext(),"yay!", Toast.LENGTH_SHORT).show();
+		create.setOnClickListener(v -> {
+			if(file.writeFile(project_package, "hello world!")) {
+				Toast.makeText(getApplicationContext(),"Файл успешно записан!", Toast.LENGTH_SHORT).show();
 			} else {
-				Toast.makeText(getApplicationContext(),"error :(", Toast.LENGTH_SHORT).show();
+				Toast.makeText(getApplicationContext(),"Ошибка :(", Toast.LENGTH_SHORT).show();
 			}
-        });
-        close.setOnClickListener(v -> finish());
+		});
+		close.setOnClickListener(v -> finish());
 
-        ArrayAdapter<String> lang_adapter = new ArrayAdapter<>(this, R.layout.spinner_item, langs);
-        lang.setAdapter(lang_adapter);
-        ArrayAdapter<String> sdk_adapter = new ArrayAdapter<>(this, R.layout.spinner_item, sdks);
-        sdk.setAdapter(sdk_adapter);
-    }
+		ArrayAdapter<String> lang_adapter = new ArrayAdapter<>(this, R.layout.spinner_item, langs);
+		lang.setAdapter(lang_adapter);
+		ArrayAdapter<String> sdk_adapter = new ArrayAdapter<>(this, R.layout.spinner_item, sdks);
+		sdk.setAdapter(sdk_adapter);
+	}
 
 }
