49638256b54e2f13521dbccd9e6c9a3cab79bfce
MrBoomDev <92123190+MrBoomDeveloper@users.noreply.github.com>
2022-05-17 10:55:48 +0500

    update BoomStudio.java

--- app/src/main/java/com/mrboomdev/androidstudio/BoomStudio.java
+++ app/src/main/java/com/mrboomdev/androidstudio/BoomStudio.java
@@ -17,7 +17,7 @@ public class BoomStudio extends Application {
 		Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
 			@Override
 			public void uncaughtException(Thread thread, Throwable throwable) {
-				Intent intent = new Intent(getApplicationContext(), CrashActivity.class);
+				Intent intent = new Intent(getApplicationContext(), CrashDialog.class);
 				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
 				intent.putExtra("error", Log.getStackTraceString(throwable));
 				PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 11111, intent, PendingIntent.FLAG_ONE_SHOT);
