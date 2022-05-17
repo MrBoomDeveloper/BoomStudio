47e4f6ab436987c90729330cb867ac7e7598db90
MrBoomDev <92123190+MrBoomDeveloper@users.noreply.github.com>
2022-05-17 20:09:31 +0500

    update Files.java

--- app/src/main/java/com/mrboomdev/androidstudio/utils/Files.java
+++ app/src/main/java/com/mrboomdev/androidstudio/utils/Files.java
@@ -20,195 +20,191 @@ import java.util.ArrayList;
 
 public class Files {
 
-    private static void createNewFile(String path) {
-        int lastSep = path.lastIndexOf(File.separator);
-        if (lastSep > 0) {
-            String dirPath = path.substring(0, lastSep);
-            makeDir(dirPath);
-        }
+	private static void createNewFile(String path) {
+		int lastSep = path.lastIndexOf(File.separator);
+		if (lastSep > 0) {
+			String dirPath = path.substring(0, lastSep);
+			makeDir(dirPath);
+		}
 
-        File file = new File(path);
+		File file = new File(path);
 
-        try {
-            if (!file.exists()) file.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+		try {
+			if (!file.exists()) file.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public static String readFile(String path) {
-        createNewFile(path);
+	public static String readFile(String path) {
+		createNewFile(path);
 
-        StringBuilder sb = new StringBuilder();
-        FileReader fr = null;
-        try {
-            fr = new FileReader(new File(path));
+		StringBuilder sb = new StringBuilder();
+		FileReader fr = null;
+		try {
+			fr = new FileReader(new File(path));
 
-            char[] buff = new char[1024];
-            int length = 0;
+			char[] buff = new char[1024];
+			int length = 0;
 
-            while ((length = fr.read(buff)) > 0) {
-                sb.append(new String(buff, 0, length));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (fr != null) {
-                try {
-                    fr.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+			while ((length = fr.read(buff)) > 0) {
+				sb.append(new String(buff, 0, length));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (fr != null) {
+				try {
+					fr.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
 
-        return sb.toString();
-    }
+		return sb.toString();
+	}
+	
+	public static void writeFile(String path, String str) throws Exception {
+		createNewFile(path);
+		FileWriter fileWriter = null;
+		try {
+			fileWriter = new FileWriter(new File(path), false);
+			fileWriter.write(str);
+			fileWriter.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new Exception("Error creating a file");
+		} finally {
+			try {
+				if (fileWriter != null) {
+					fileWriter.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+				throw new Exception("Error creating a file");
+			}
+		}
+	}
 
-    public static boolean writeFile(String path, String str) {
-        createNewFile(path);
-        FileWriter fileWriter = null;
+	public static void copyFile(String sourcePath, String destPath) {
+		if (!isExistFile(sourcePath)) return;
+		createNewFile(destPath);
 
-        try {
-            fileWriter = new FileWriter(new File(path), false);
-            fileWriter.write(str);
-            fileWriter.flush();
-            return true;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        } finally {
-            try {
-                if (fileWriter != null)
-{
-                    fileWriter.close();
-                    return true;
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-                return false;
-            }
-        }
-    }
+		FileInputStream fis = null;
+		FileOutputStream fos = null;
 
-    public static void copyFile(String sourcePath, String destPath) {
-        if (!isExistFile(sourcePath)) return;
-        createNewFile(destPath);
+		try {
+			fis = new FileInputStream(sourcePath);
+			fos = new FileOutputStream(destPath, false);
 
-        FileInputStream fis = null;
-        FileOutputStream fos = null;
+			byte[] buff = new byte[1024];
+			int length = 0;
 
-        try {
-            fis = new FileInputStream(sourcePath);
-            fos = new FileOutputStream(destPath, false);
+			while ((length = fis.read(buff)) > 0) {
+				fos.write(buff, 0, length);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (fis != null) {
+				try {
+					fis.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (fos != null) {
+				try {
+					fos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
-            byte[] buff = new byte[1024];
-            int length = 0;
+	public static void copyDir(String oldPath, String newPath) {
+		File oldFile = new File(oldPath);
+		File[] files = oldFile.listFiles();
+		File newFile = new File(newPath);
+		if (!newFile.exists()) {
+			newFile.mkdirs();
+		}
+		for (File file : files) {
+			if (file.isFile()) {
+				copyFile(file.getPath(), newPath + "/" + file.getName());
+			} else if (file.isDirectory()) {
+				copyDir(file.getPath(), newPath + "/" + file.getName());
+			}
+		}
+	}
 
-            while ((length = fis.read(buff)) > 0) {
-                fos.write(buff, 0, length);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (fis != null) {
-                try {
-                    fis.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (fos != null) {
-                try {
-                    fos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+	public static void moveFile(String sourcePath, String destPath) {
+		copyFile(sourcePath, destPath);
+		deleteFile(sourcePath);
+	}
 
-    public static void copyDir(String oldPath, String newPath) {
-        File oldFile = new File(oldPath);
-        File[] files = oldFile.listFiles();
-        File newFile = new File(newPath);
-        if (!newFile.exists()) {
-            newFile.mkdirs();
-        }
-        for (File file : files) {
-            if (file.isFile()) {
-                copyFile(file.getPath(), newPath + "/" + file.getName());
-            } else if (file.isDirectory()) {
-                copyDir(file.getPath(), newPath + "/" + file.getName());
-            }
-        }
-    }
+	public static void deleteFile(String path) {
+		File file = new File(path);
 
-    public static void moveFile(String sourcePath, String destPath) {
-        copyFile(sourcePath, destPath);
-        deleteFile(sourcePath);
-    }
+		if (!file.exists()) return;
 
-    public static void deleteFile(String path) {
-        File file = new File(path);
+		if (file.isFile()) {
+			file.delete();
+			return;
+		}
 
-        if (!file.exists()) return;
+		File[] fileArr = file.listFiles();
 
-        if (file.isFile()) {
-            file.delete();
-            return;
-        }
+		if (fileArr != null) {
+			for (File subFile : fileArr) {
+				if (subFile.isDirectory()) {
+					deleteFile(subFile.getAbsolutePath());
+				}
 
-        File[] fileArr = file.listFiles();
+				if (subFile.isFile()) {
+					subFile.delete();
+				}
+			}
+		}
 
-        if (fileArr != null) {
-            for (File subFile : fileArr) {
-                if (subFile.isDirectory()) {
-                    deleteFile(subFile.getAbsolutePath());
-                }
+		file.delete();
+	}
 
-                if (subFile.isFile()) {
-                    subFile.delete();
-                }
-            }
-        }
+	public static boolean isExistFile(String path) {
+		File file = new File(path);
+		return file.exists();
+	}
 
-        file.delete();
-    }
+	public static void makeDir(String path) {
+		if (!isExistFile(path)) {
+			File file = new File(path);
+			file.mkdirs();
+		}
+	}
 
-    public static boolean isExistFile(String path) {
-        File file = new File(path);
-        return file.exists();
-    }
+	public static void listDir(String path, ArrayList<String> list) {
+		File dir = new File(path);
+		if (!dir.exists() || dir.isFile()) return;
 
-    public static void makeDir(String path) {
-        if (!isExistFile(path)) {
-            File file = new File(path);
-            file.mkdirs();
-        }
-    }
+		File[] listFiles = dir.listFiles();
+		if (listFiles == null || listFiles.length <= 0) return;
 
-    public static void listDir(String path, ArrayList<String> list) {
-        File dir = new File(path);
-        if (!dir.exists() || dir.isFile()) return;
+		if (list == null) return;
+		list.clear();
+		for (File file : listFiles) {
+			list.add(file.getAbsolutePath());
+		}
+	}
 
-        File[] listFiles = dir.listFiles();
-        if (listFiles == null || listFiles.length <= 0) return;
-
-        if (list == null) return;
-        list.clear();
-        for (File file : listFiles) {
-            list.add(file.getAbsolutePath());
-        }
-    }
-
-    public static boolean isFile(String path) {
-        if (!isExistFile(path)) return false;
-        return new File(path).isFile();
-    }
-    
-    public String uriToPath(String uri) {
-    	String result = uri.replace("/tree/raw:", "").replace("/tree/primary:", "/storage/emulated/0/").replace("/tree/", "/storage/").replace(":", "/").replace("/storage/home/", "/storage/emulated/0/Documents");
-    return result;
-    }
+	public static boolean isFile(String path) {
+		if (!isExistFile(path)) return false;
+		return new File(path).isFile();
+	}
+	
+	public String uriToPath(String uri) {
+		String result = uri.replace("/tree/raw:", "").replace("/tree/primary:", "/storage/emulated/0/").replace("/tree/", "/storage/").replace(":", "/").replace("/storage/home/", "/storage/emulated/0/Documents");
+	return result;
+	}
 }
